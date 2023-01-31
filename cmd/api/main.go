8389c8a3361498222fae2afafd713acce2f2840a
Josh Duffney <jduffney@microsoft.com>
2023-01-31 08:48:04 -0600

    endpoint with defaultServeMux

---
+++ cmd/api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/v1/healthcheck", healthcheck)
+
+	err := http.ListenAndServe(":4000", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "status: available")
+	fmt.Fprintf(w, "environment: %s\n", "dev")
+	fmt.Fprintf(w, "version: %s\n", "1.0.0")
+}
